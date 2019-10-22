35e0a83094de8b9fdee56a859f8b5235cb165469
shizhiqiang <icrank@qq.com>
2019-10-22 22:16:02 +0800

    节流防抖划重点

--- fun-tool/index.js
+++ fun-tool/index.js
@@ -1,5 +1,6 @@
 /**
  * 函数防抖 (只执行最后一次点击)
+ * 场景：验证邮箱密码、搜索框失去焦点开始搜索
  * @param fn
  * @param delay
  * @returns {Function}
@@ -20,7 +21,8 @@ export const Debounce = (fn, t) => {
   }
 }
 /**
- * 函数节流
+ * 函数节流（函数执行后，规定时间内无法再次调用）
+ * 场景：定时检测、重复提交
  * @param fn
  * @param interval
  * @returns {Function}
